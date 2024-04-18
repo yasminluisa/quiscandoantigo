a3d426ac24c5cf3b3f404cd3474593b4e3b3fdb9
yasminluisa <126296748+yasminluisa@users.noreply.github.com>
2024-04-18 19:44:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayMenu(){
+    const menuList = document.createElementById("menu-list");
+    menuItems.forEach(item =>{
+        const li =document.getElementById("menu-list");
+    })
+
+
+
+}
